f661a487870b960824bd89c0c71e233c69a286b1
digitalh2o <digitalh2o@gmail.com>
2018-07-03 21:48:51 -0500

    Set up redux-thunk and fetching user info succcessfully from back-end with redux. Slight styling next before continuing.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,12 +1,23 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import * as actions from "../actions";
 
-const App = () => {
-  return (
-    <div>
-      Hi There~
-      <a href="/auth/discord">Log In with discord</a>
-    </div>
-  );
-};
+class App extends Component {
+  componentDidMount() {
+    this.props.fetchUser();
+  }
 
-export default App;
+  render() {
+    return (
+      <div>
+        Hi There~
+        <a href="/auth/discord">Log In with discord</a>
+      </div>
+    );
+  }
+}
+
+export default connect(
+  null,
+  actions
+)(App);
