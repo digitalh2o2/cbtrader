af8ae923f69ef58dfb39abca9381065e300fbd66
digitalh2o <digitalh2o@gmail.com>
2018-07-21 12:22:16 -0500

    Updated Listing model to associate with user

--- models/Listing.js
+++ models/Listing.js
@@ -1,10 +1,12 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const listingSchema = new Schema({
   pokemonName: String,
   pokemonLevel: String,
-  pokemonOrigin: String
-})
+  pokemonOrigin: String,
+  _user: { type: Scehma.Types.ObjectId, ref: "User" },
+  datePosted: Date
+});
 
-mongoose.model('listings', listingSchema);
+mongoose.model("listings", listingSchema);
